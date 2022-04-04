0398e0bb1517c0e0b65455cb06f6bba8e9ecf7e1
noco <k-nitrate.1002@ezweb.ne.jp>
2022-04-04 20:57:36 +0900

    Update blc8.hs

--- blc8.hs
+++ blc8.hs
@@ -4,64 +4,56 @@ import System.IO (hSetBinaryMode, hSetBuffering, stdin, stdout, BufferMode (NoBu
 
 data Expr = I Int | L Expr | A Expr Expr
 
-app :: Expr -> Expr -> Expr
-app (L x) y = beta 0 x y
+a :: Expr -> Expr -> Expr
+a (L x) y = b 0 x y
     where
-        beta :: Int -> Expr -> Expr -> Expr
-        beta i (I j) x
+        b :: Int -> Expr -> Expr -> Expr
+        b i (I j) x
             | i < j = I $ j - 1
-            | i == j = beta' 0 i x
+            | i == j = b' 0 i x
             | otherwise = I j
-        beta i (L x) y = L $ beta (i + 1) x y
-        beta i (A x y) z = app (beta i x z) $ beta i y z
+        b i (L x) y = L $ b (i + 1) x y
+        b i (A x y) z = a (b i x z) $ b i y z
 
-        beta' :: Int -> Int -> Expr -> Expr
-        beta' i j (I k)
+        b' :: Int -> Int -> Expr -> Expr
+        b' i j (I k)
             | i <= k = I $ j + k
             | otherwise = I k
-        beta' i j (L x) = L $ beta' (i + 1) j x
-        beta' i j (A x y) = app (beta' i j x) $ beta' i j y
-app x y = A x y
+        b' i j (L x) = L $ b' (i + 1) j x
+        b' i j (A x y) = a (b' i j x) $ b' i j y
+a x y = A x y
 
 parse :: [Char] -> Expr
 parse = head . parse' . filter (`elem` "01") . concatMap (takeWhile (/= '#')) . lines
     where
         parse' :: [Char] -> [Expr]
         parse' ('0' : '0' : t) = (\ (x : t) -> L x : t) $ parse' t
-        parse' ('0' : '1' : t) = (\ (x : y : t) -> app x y : t) $ parse' t
+        parse' ('0' : '1' : t) = (\ (x : y : t) -> a x y : t) $ parse' t
         parse' ('1' : '0' : t) = I 0 : parse' t
         parse' ('1' : t) = (\ (I i : t) -> I (i + 1) : t) $ parse' t
         parse' _ = []
 
 church8 :: [Char] -> Expr
-church8 = cList . map (cList . map cBool . toBin8)
+church8 = cL . map (cL . map cB . reverse . take 8 . unfoldr (\ x -> Just (odd x, div x 2)) . fromEnum)
     where
-        cList :: [Expr] -> Expr
-        cList = foldr (\ x -> L . app (A (I 0) x)) $ L $ L $ I 0
+        cL :: [Expr] -> Expr
+        cL = foldr (\ x -> L . a (A (I 0) x)) $ L $ L $ I 0
 
-        cBool :: Bool -> Expr
-        cBool = L . L . I . fromEnum
-
-        toBin8 :: Char -> [Bool]
-        toBin8 = reverse . take 8 . unfoldr (\ x -> Just (odd x, div x 2)) . fromEnum
+        cB :: Bool -> Expr
+        cB = L . L . I . fromEnum
 
 unchurch8 :: Expr -> [Char]
-unchurch8 = map (fromBin8 . map uncBool . uncList) . uncList
+unchurch8 = map (toEnum . foldl (\ x y -> 2 * x + fromEnum y) 0 . take 8 . map uncB . uncL) . uncL
     where
-        fromBin8 :: [Bool] -> Char
-        fromBin8 = toEnum . foldl (\ x y -> 2 * x + fromEnum y) 0 . take 8
+        uncB :: Expr -> Bool
+        uncB (L (L (I 1))) = True
+        uncB (L (L (I 0))) = False
+        uncB x = uncB $ a (a x $ L $ L $ I 1) $ L $ L $ I 0
 
-        uncBool :: Expr -> Bool
-        uncBool (L (L (I 1))) = True
-        uncBool (L (L (I 0))) = False
-        uncBool x = uncBool $ app (app x $ L $ L $ I 1) $ L $ L $ I 0
-
-        uncList :: Expr -> [Expr]
-        uncList (L (L (I 0))) = []
-        uncList (L (A (A (I 0) x) y)) = x : uncList y
-        uncList x
-            | uncBool $ app (app x $ L $ L $ L $ L $ L $ I 0) $ L $ L $ I 1 = []
-            | otherwise = app x (L $ L $ I 1) : uncList (app x $ L $ L $ I 0)
+        uncL :: Expr -> [Expr]
+        uncL x
+            | uncB $ a (a x $ L $ L $ L $ L $ L $ I 0) $ L $ L $ I 1 = []
+            | otherwise = a x (L $ L $ I 1) : uncL (a x $ L $ L $ I 0)
 
 main :: IO ()
 main = do
@@ -71,7 +63,7 @@ main = do
     hSetBinaryMode stdout b
     hSetBuffering stdout NoBuffering
     input <- getContents
-    putStr $ unchurch8 $ foldl1 (flip app) $ church8 input : map parse codes
+    putStr $ unchurch8 $ foldl1 (flip a) $ church8 input : map parse codes
     where
         f :: [[Char]] -> IO (Bool, [[Char]])
         f ("-b" : x) = sequence (True, sequence $ g x)
